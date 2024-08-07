a4670219988a81bfb1d029de62e7da1a6468ef2b
Nidhi6669 <nidhikumari.professional@gmail.com>
2024-08-07 14:48:27 +0530

    Update Context.jsx

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -37,9 +37,7 @@ const ContextProvider = (props)=>{
             setRecentPrompt(input);
             response = await run (input)
         }
-        // setRecentPrompt(input)
-        // setPrevPrompts(prev=>[...prev,input])
-        // const response =await run(input)
+      
         let responseArray= response.split("**")
         let newResponse="";
         for(let i=0;i<responseArray.length;i++){
